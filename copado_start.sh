66539eb3c741bd348c9d32c36ac51483eff2f593
vcosquico <41107591+vcosquico@users.noreply.github.com>
2018-08-31 14:44:46 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -2,19 +2,19 @@
 
 echo "[c1p worker job] invoked copado job from vcosquico repository"
 
-notify_status "Running" "20"
+notify_status "20" "Running" 
 sleep 10s
 
-notify_status "hello Luis, thanks from Jesus" "40"
+notify_status "40" "hello Luis, thanks from Jesus" 
 sleep 10s
 
-notify_status "Running" "60"
+notify_status "60" "Running" 
 sleep 10s
 
-notify_status "all your base are belong to us" "80"
+notify_status "80" "all your base are belong to us" 
 sleep 10s
 
-notify_status "copado rulez" "100"
+notify_status "100" "copado rulez" 
 sleep 10s
 
 echo "[c1p worker job] done! success"
