c3705a8f2a3a27ed3dcd0e15631751f7c21f9f8e
vcosquico <41107591+vcosquico@users.noreply.github.com>
2018-08-31 14:16:38 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -2,19 +2,19 @@
 
 echo "[c1p worker job] invoked copado job from vcosquico repository"
 
-notify_status "20" "20"
+notify_status "20" "a"
 sleep 10s
 
-notify_status "40" "40"
+notify_status "40" "b"
 sleep 10s
 
-notify_status "60" "60"
+notify_status "60" "c"
 sleep 10s
 
-notify_status "80" "80"
+notify_status "80" "d"
 sleep 10s
 
-notify_status "100" "100"
+notify_status "100" "e"
 sleep 10s
 
 echo "[c1p worker job] done! success"
