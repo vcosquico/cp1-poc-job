fa574cb5e4a4316549a05d51fee508ecffa0b954
vcosquico <41107591+vcosquico@users.noreply.github.com>
2018-09-04 15:43:51 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -5,7 +5,7 @@ printenv
 
 notify_status "Retrieving_data" "20" 
 curl "${COPADO_SF_SERVICE_ENDPOINT}query?q=SELECT+name,+StageName+from+opportunity" \
--H 'Authorization: Bearer $COPADO_SF_AUTH_HEADER' \
+-H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' \
 | jq -c -r '.records[] | [.Name, .StageName] | @csv' > opportunities.csv
 sleep 2s
 
