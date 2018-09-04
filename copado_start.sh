1119cb397b44704291940f8e4e8b0314ece03984
vcosquico <41107591+vcosquico@users.noreply.github.com>
2018-09-04 13:42:15 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -3,19 +3,19 @@
 echo "[c1p worker job] invoked copado job from vcosquico repository"
 printenv
 
-notify_status "Running" "20" 
-sleep 10s
+notify_status "Retrieving data" "20" 
+curl https://copado-1-platform-dev-01-dev-ed.my.salesforce.com/services/data/v20.0/query\?q\=SELECT+name,+StageName+from+opportunity -H "Authorization: Bearer 00D1t000000Eo4i\!ARAAQA9Bx7zWKliNpxT1afs4ll_KeOcVjEDn4Qy853uVo6rDJ815RTu_VJM7Cfvwm.nrTrFr4GebKq.9JoWQKtiTW3LeamFN" | jq -c -r '.records[] | [.Name, .StageName] | @csv' > opportunities.csv
+sleep 2s
 
-notify_status "Running..." "40"  
-sleep 10s
+notify_status "Compressing data" "40"  
+zip --password copado opportinities.zip opportunities.csv
+sleep 2s
 
-notify_status "Running......" "60" 
-sleep 10s
-
-notify_status "Running........." "80" 
-sleep 10s
+notify_status "Uploading data" "60" 
+curl -T opportunities.csv -u copado:df1Cpd0 "ftp://ftp.copado.com/opportunities-$(date +%s).csv"
+sleep 2s
 
 notify_status "Copado rulez" "100" 
-sleep 10s
+sleep 2s
 
 echo "[c1p worker job] done! success"
