2b04412e76437848cb4f1013d6d719175c4a7ef8
vcosquico <41107591+vcosquico@users.noreply.github.com>
2018-09-05 16:43:12 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -22,15 +22,18 @@ sleep 2s
 
 #
 # before you will need:
-# 1.- Enable oauth access to yuor drive account
+# 1.- Enable oauth access to your drive account
 # 2.- request a CODE https://accounts.google.com/o/oauth2/auth?client_id=XXX&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive
 # 3.- login to get the access_token curl -XPOST --data "code=CODE&client_id=XXX&client_secret=YYY&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code" "https://accounts.google.com/o/oauth2/token"
 notify_status "Uploading_data_to_Google_Drive" "80" 
 echo "Uploading GDrive data"
+# get access token
 curl -sS -XPOST --data 'grant_type=refresh_token&client_id=864421843858-g6b3ngvrpg8p9j2kt03rv0l0h0kteuhn.apps.googleusercontent.com&client_secret=pUvKreCL4nLnYAaj_xmGyGa9&refresh_token=1/24qD4kf0lU-91tfazVp161zPCDkrwtX5PrQD4jh0q4tWM7WCt5xWfSfTcLTfYUnX' "https://accounts.google.com/o/oauth2/token" | jq -cr '.access_token' > ./.drive_token
 cat ./.drive_token
-#curl -v -XPOST "https://www.googleapis.com/upload/drive/v3/files?uploadType=media&name=opportunities$(date +%s).zip" -H "Content-Type: application/zip, application/octet-stream" -H "Authorization: Bearer $(cat ./.drive_token)" -d@opportunities.zip -o ./.metadata.json
-curl -v -XPOST "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart" -H "Content-Type: multipart/mixed" -H "Authorization: Bearer $(cat ./.drive_token)" -F "file=@./opportunities.zip" -F "metadata={name : 'opportunities.zip'};type=application/json;charset=UTF-8" -F "metadata={parents : '[{kind:'drive#parentReference',id:'1W-wTdGs6duUDWvGfZ0B7C3itO-W5mU_V'}]'}"
+# put file metadata
+curl -sD - -XPOST "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&part=snippet" -H "Content-Type: application/json" -H "Authorization: Bearer $(cat ./.drive_token)" -d '{"name":"opportunities'-"$(date +%s)"'.zip"}' | grep x-guploader-uploadid:  | tr -d '\r' | sed -En 's/^x-guploader-uploadid: (.*)/\1/p' | tee ./.fileid 
+# put file
+curl -Lv -XPOST "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&part=snippet&upload_id=$(cat ./.fileid)" -H "Authorization: Bearer $(cat ./.drive_token)" -H "Content-type: application/zip" --data-binary @opportunities.zip
 
 notify_status "Copado_rulez" "100" 
 echo "Finish"
