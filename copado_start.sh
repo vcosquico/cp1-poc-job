19d7424a5f973ecb8796486735b2696bb057f863
vcosquico <41107591+vcosquico@users.noreply.github.com>
2018-09-05 11:52:36 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -20,10 +20,16 @@ echo "Uploading FTP data"
 curl -sS -T opportunities.zip -u "$FTP_USER":"$FTP_PWD" "$FTP_URL3/opportunities-$(date +%s).zip"
 sleep 2s
 
+#
+# before you will need:
+# 1.- Enable oauth access to yuor drive account
+# 2.- request a CODE https://accounts.google.com/o/oauth2/auth?client_id=XXX&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive
+# 3.- login to get the access_token curl -XPOST --data "code=CODE&client_id=XXX&client_secret=YYY&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code" "https://accounts.google.com/o/oauth2/token"
 notify_status "Uploading_data_to_Google_Drive" "80" 
 echo "Uploading GDrive data"
 curl -sS -XPOST --data 'client_id=864421843858-g6b3ngvrpg8p9j2kt03rv0l0h0kteuhn.apps.googleusercontent.com&client_secret=pUvKreCL4nLnYAaj_xmGyGa9&refresh_token=1/24qD4kf0lU-91tfazVp161zPCDkrwtX5PrQD4jh0q4tWM7WCt5xWfSfTcLTfYUnX' "https://accounts.google.com/o/oauth2/token?grant_type=refresh_token" | jq -cr '.access_token' > ./.drive_token
 cat ./.drive_token
+curl -sS -XPOST "https://www.googleapis.com/upload/drive/v3/files?uploadType=media&name=opportunities$(date +%s).zip" -H "Content-Type: application/zip, application/octet-stream" -H "Authorization: Bearer $(cat ./.drive_token)" -d@opportunities.zip
 
 notify_status "Copado_rulez" "100" 
 echo "Finish"
