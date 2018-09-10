821ed288b2d797603353eb2f55805df5bdc30614
vcosquico <41107591+vcosquico@users.noreply.github.com>
2018-09-10 14:12:52 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -15,8 +15,9 @@ curl -sS "${COPADO_SF_SERVICE_ENDPOINT}query?q=SELECT+Id,+Name,+StageName,+Accou
 curl -sS "${COPADO_SF_SERVICE_ENDPOINT}query?q=Select+id,+ContentDocumentId,+ContentDocument.LatestPublishedVersionId+from+ContentDocumentLink+where+LinkedEntityId+IN+($(cat ./.opportunities.id))" -H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' | jq -c -r '.records[] | .ContentDocument.LatestPublishedVersionId' > ./.content.doc.id
 while read docId; do
   echo "downloading $docId"
+  mkdir attachments/$docId
   curl -sS "${COPADO_SF_SERVICE_ENDPOINT}sobjects/ContentVersion/$docId" -H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' | jq -r -c .PathOnClient > ./.curr.file.name
-  curl "${COPADO_SF_SERVICE_ENDPOINT}sobjects/ContentVersion/$docId/VersionData"  -H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' -o ./attachments/$(cat ./.curr.file.name)
+  curl "${COPADO_SF_SERVICE_ENDPOINT}sobjects/ContentVersion/$docId/VersionData"  -H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' -o ./attachments/$docId/$(cat ./.curr.file.name)
 done <./.content.doc.id
 sleep 2s
 
