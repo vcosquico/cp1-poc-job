bb99c99e3ba67bee783ffbe1cae8f3988096af05
vcosquico <41107591+vcosquico@users.noreply.github.com>
2018-09-17 11:17:35 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "[c1p worker job] invoked copado job from vcosquico repository"
-printenv
 
 notify_status "Retrieving_data" "20" 
 echo "Retrieving data"
@@ -12,6 +11,7 @@ curl -sS "${COPADO_SF_SERVICE_ENDPOINT}query?q=SELECT+Id,+Name,+StageName,+Accou
 
 notify_status "Retrieving_attachments" "40"
 # download all attachment files for the opportunities
+echo "Downloading files"
 mkdir attachments
 curl -sS "${COPADO_SF_SERVICE_ENDPOINT}query?q=SELECT+Id,+Name,+StageName,+AccountId,+Account.Name,+(select+Id,+Pricebookentry.product2.name+from+OpportunityLineItems)from+opportunity+WHERE+StageName+=+'Closed+Won'" -H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' | jq -c -r '.records[] | [.Id]' | sed "s/\"/'/g" | sed "s/[^a-zA-Z0-9']/ /g" | tr '\n' ',' | tr -d " " | sed 's/.$//' > ./.opportunities.id
 curl -sS "${COPADO_SF_SERVICE_ENDPOINT}query?q=Select+id,+ContentDocumentId,+ContentDocument.LatestPublishedVersionId+from+ContentDocumentLink+where+LinkedEntityId+IN+($(cat ./.opportunities.id))" -H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' | jq -c -r '.records[] | .ContentDocument.LatestPublishedVersionId' > ./.content.doc.id
@@ -19,14 +19,14 @@ while read docId; do
   echo "downloading $docId"
   mkdir attachments/$docId
   curl -sS "${COPADO_SF_SERVICE_ENDPOINT}sobjects/ContentVersion/$docId" -H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' | jq -r -c .PathOnClient > ./.curr.file.name
-  curl "${COPADO_SF_SERVICE_ENDPOINT}sobjects/ContentVersion/$docId/VersionData"  -H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' -o "./attachments/$docId/$(cat ./.curr.file.name)"
+  curl -sS "${COPADO_SF_SERVICE_ENDPOINT}sobjects/ContentVersion/$docId/VersionData"  -H 'Authorization: Bearer '"$COPADO_SF_AUTH_HEADER"'' -o "./attachments/$docId/$(cat ./.curr.file.name)"
 done <./.content.doc.id
 
 notify_status "Compressing_data" "50"
 echo "Compressing data"
 # zip all the attachments and the opportinities csv
 ls attachments/
-zip -r --password copado opportunities.zip opportunities.csv attachments/* ./.opportunities.id ./.content.doc.id
+zip -qr --password copado opportunities.zip opportunities.csv attachments/* ./.opportunities.id ./.content.doc.id
 
 notify_status "Uploading_data_to_FTP" "60" 
 echo "Uploading FTP data"
@@ -45,7 +45,7 @@ curl -sS -XPOST --data "grant_type=refresh_token&client_id=$GDRIVE_CLIENT_ID&cli
 # put file metadata
 curl -sD - -XPOST "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&part=snippet" -H "Content-Type: application/json" -H "Authorization: Bearer $(cat ./.drive_token)" -d "{\"name\":\"opportunities-$(date +%s).zip\"}" | tr -d '\r' | sed -En 's/^X-GUploader-UploadID: (.*)/\1/p' | tee ./.fileid 
 # put fil
-curl -Lv -XPOST "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&part=snippet&upload_id=$(cat ./.fileid)" -H "Authorization: Bearer $(cat ./.drive_token)" -H "Content-type: application/zip" --data-binary @opportunities.zip
+curl -sLv -XPOST "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&part=snippet&upload_id=$(cat ./.fileid)" -H "Authorization: Bearer $(cat ./.drive_token)" -H "Content-type: application/zip" --data-binary @opportunities.zip
 
 notify_status "Copado_rulez" "100" 
 echo "Finish"
